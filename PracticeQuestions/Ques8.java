3ed06aaf93da366f0702dbf641316c60de01ed10
Harman <harmandhaliwall147@gmail.com>
2026-08-10 13:52:39 +0530

    done questions

--- PracticeQuestions/Ques8.java
+++ PracticeQuestions/Ques8.java
@@ -1,5 +1,31 @@
-package PracticeQuestions;
+// Q8. Write a Java program that displays grades based on percentage:
+// 90+ → A+
+// 80–89 → A
+// 70–79 → B
+// 60–69 → C
+// Below 60 → Fail
+
+import java.util.Scanner;
 
 public class Ques8 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter percentage: ");
+        int percentage = sc.nextInt();
+
+        if(percentage >= 90){
+            System.out.println("Grade: A+");
+        }else if(percentage >= 80) {
+            System.out.println("Grade: A");
+        }else if(percentage >= 70) {
+            System.out.println("Grade: B");
+        }else if(percentage >= 60) {
+            System.out.println("Grade: C");
+        }else{
+            System.out.println("Fail");
+        }
+
+        sc.close();
+    }
 }
--- PracticeQuestions/Ques9.java
+++ PracticeQuestions/Ques9.java
@@ -1,5 +1,11 @@
-package PracticeQuestions;
+// Q9. Print all even numbers from 1 to 100.
 
 public class Ques9 {
-    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
