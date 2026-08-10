8688a4d0a807729019d1c445d0b9d6180b492e59
Harman <harmandhaliwall147@gmail.com>
2026-08-10 13:46:29 +0530

    done practice questions

--- PracticeQuestions/Ques7.java
+++ PracticeQuestions/Ques7.java
@@ -1,5 +1,20 @@
 // ------------ Write a program to check whether a year is a Leap Year -----------
 
+import java.util.Scanner;
+
 public class Ques7 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+            System.out.println("Leap Year");
+        }else{
+            System.out.println("Not a Leap Year");
+        }
+
+        sc.close();
+    }
 }
