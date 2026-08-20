7ec90f881200d71edd65f0e714b47375f6209107
Harman <harmandhaliwall147@gmail.com>
2026-08-20 18:17:01 +0530

    Delete first Node

---
+++ Day-18/FirstNodeDel.java
@@ -0,0 +1,53 @@
+public class FirstNodeDel {
+
+    static class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data){
+            this.data=data;
+        }
+    }
+
+    static Node deleteFirst(Node head){
+
+        if(head.next!=null){
+            head=head.next;
+            head.prev=null;
+        }
+
+        return head;
+    }
+    
+    static void print(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+ " ");
+            temp=temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        Node fourth = new Node(40);
+
+        head.next = second;
+        second.prev = head;
+        second.next=third;
+        third.prev=second;
+        third.next=fourth;
+        fourth.prev=third;
+
+        System.out.println("Original List:");
+        print(head);
+        head = deleteFirst(head);
+        System.out.println("\nAfter deleting first node :");
+        print(head);
+        
+
+
+    }
+}
