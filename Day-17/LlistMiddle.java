5d4f37de4ad6232ccfee868d1a6045893001ed12
Harman <harmandhaliwall147@gmail.com>
2026-09-01 11:42:06 +0530

    Codes updated

--- Day-17/LlistMiddle.java
+++ Day-17/LlistMiddle.java
@@ -1,3 +1,31 @@
 public class LlistMiddle {
     
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next=null;
+        }
+    }
+    public static Node findMid(Node head){
+        // floyd's algorithm
+        
+        if(head==null) return head;
+        Node slow=head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public static void main(String[] args){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        System.out.println("mid: "+findMid(head).data);
+    }
 }
--- Day-17/LlistReverse.java
+++ Day-17/LlistReverse.java
@@ -1,3 +1,46 @@
 public class LlistReverse {
-    
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next=null;
+        }
+    }
+    public static Node reverse(Node head){
+        Node curr=head;
+        Node prev = null;
+        Node nextptr = null;
+        while(curr!=null){
+            nextptr=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nextptr;
+        }
+        return prev;
+    }
+
+    public static void print(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        System.out.println("----------Original Linked List----------");
+        print(head);
+
+        head = reverse(head);
+        System.out.println("----------Reversed Linked List----------");
+        print(head);
+    }
 }
