61fb6bd90147fa36bab9430091b5e52add3e3e81
Harman <harmandhaliwall147@gmail.com>
2026-09-08 12:39:35 +0530

    LeetCode - 328

---
+++ LinkedListProblems/Q3.java
@@ -0,0 +1,78 @@
+// LEETCODE - 328
+// ODD EVEN LINKED LIST
+
+
+public class Q3 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+
+        ListNode curr = head;
+
+        while (curr != null) {
+            System.out.print(curr.val);
+
+            if (curr.next != null) {
+                System.out.print(" -> ");
+            }
+
+            curr = curr.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        System.out.println("Before:");
+        printList(n1);
+
+        ListNode result = oddEvenList(n1);
+
+        System.out.println("After:");
+        printList(result);
+    }
+}
