4b2113fb4fca6964924fc95e93919392646264d1
Harman <harmandhaliwall147@gmail.com>
2026-09-09 12:18:25 +0530

    LeetCode 24

---
+++ LinkedListProblems/Q10.java
@@ -0,0 +1,66 @@
+// LEETCODE - 24
+// Swap nodes in pairs
+
+
+public class Q10 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        while(prev.next != null && prev.next.next != null){
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val);
+            if (curr.next != null) {
+                System.out.print(" -> ");
+            }
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        head = swapPairs(head);
+
+        System.out.println("After swapping pairs:");
+        printList(head);
+    }
+}
