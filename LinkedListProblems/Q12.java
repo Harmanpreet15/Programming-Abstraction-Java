e2aa307cd44f154df557ff4339b07813c7c0d88b
Harman <harmandhaliwall147@gmail.com>
2026-09-09 12:41:32 +0530

    LeetCode 141

---
+++ LinkedListProblems/Q12.java
@@ -0,0 +1,50 @@
+
+// LEETCODE - 141
+// ---------------- LinkedList Cycle -------------------
+
+
+public class Q12 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        head.next.next.next.next = head.next.next;
+
+        boolean result = hasCycle(head);
+
+        System.out.println("Does the linked list contain a cycle? " + result);
+    }
+}
