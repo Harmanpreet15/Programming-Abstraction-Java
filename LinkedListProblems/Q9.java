e03d3a455437805b2440f64c92c3db2c96db8505
Harman <harmandhaliwall147@gmail.com>
2026-09-09 11:12:55 +0530

    LeetCode 21

---
+++ LinkedListProblems/Q9.java
@@ -0,0 +1,75 @@
+// LEETCODE - 21
+// Merge two sorted list
+
+
+public class Q9 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                curr.next = list1;
+                list1 = list1.next;
+            }else{
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if(list1 != null){
+            curr.next = list1;
+        }else{
+            curr.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val);
+            if (curr.next != null) {
+                System.out.print(" -> ");
+            }
+            curr = curr.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+
+        System.out.println("List 1:");
+        printList(list1);
+
+        System.out.println("List 2:");
+        printList(list2);
+
+        // Merge
+        ListNode result = mergeTwoLists(list1, list2);
+
+        System.out.println("Merged list:");
+        printList(result);
+    }
+}
