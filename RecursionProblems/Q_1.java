f5689c84705b724027756807e8f45b05c58dacae
Harman <harmandhaliwall147@gmail.com>
2026-09-10 11:26:55 +0530

    Factorial basic problem

---
+++ RecursionProblems/Q_1.java
@@ -0,0 +1,15 @@
+// FACTORIAL PROBLEM 
+
+public class Q_1 {
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+
+        return n * factorial(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(5));
+    }
+}
