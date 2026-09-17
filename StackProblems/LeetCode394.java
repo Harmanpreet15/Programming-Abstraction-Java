d58c5e1f06e4f37c9860630602b9d68129fc2da7
Harman <harmandhaliwall147@gmail.com>
2026-09-17 12:51:22 +0530

    Leetcode solved

---
+++ StackProblems/LeetCode394.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class LeetCode394 {
+
+    public static String decodeString(String s) {
+
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+
+        StringBuilder current = new StringBuilder();
+        int num = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+
+                countStack.push(num);
+                stringStack.push(current.toString());
+
+                num = 0;
+                current.setLength(0);
+            }
+
+            else if (ch == ']') {
+
+                int count = countStack.pop();
+                String previous = stringStack.pop();
+
+                StringBuilder temp = new StringBuilder(previous);
+
+                for (int i = 0; i < count; i++) {
+                    temp.append(current);
+                }
+
+                current = temp;
+            }
+
+            else {
+                current.append(ch);
+            }
+        }
+
+        return current.toString();
+    }
+
+
+    public static void main(String[] args) {
+
+        String s = "3[a2[c]]";
+
+        String answer = decodeString(s);
+
+        System.out.println("Decoded String: " + answer);
+    }
+}
+
