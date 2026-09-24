5b301c7aac06c9d943cb212e1342c7d6110d4007
Harman <harmandhaliwall147@gmail.com>
2026-09-24 17:22:59 +0530

    Problem 2

---
+++ Day-22/Problem2.java
@@ -0,0 +1,18 @@
+// Fibonacci series
+
+public class Problem2 {
+    public static void main(String[] args) {
+        int n = 7;
+
+        int a = 0;
+        int b = 1;
+
+        for(int i=1; i<=n; i++){
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
