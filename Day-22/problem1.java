b0d2eb4901d5b76ec4c88621f9e54e573d298462
Harman <harmandhaliwall147@gmail.com>
2026-09-24 17:19:11 +0530

    Problem 1

---
+++ Day-22/problem1.java
@@ -0,0 +1,17 @@
+
+public class problem1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            if(i<=3){
+                for(int j = 1; j<=i; j++){
+                    System.out.print(j + " ");
+                }
+            }else{
+                for(int j=i; j>=1; j--){
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
