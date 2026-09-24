b0feb0c015673a942d7321c6bbc42cb1d5a84d3d
Harman <harmandhaliwall147@gmail.com>
2026-09-24 17:45:12 +0530

    Problem : 5

---
+++ Day-22/Problem5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        int n = 122563;
+
+        String result = "";
+
+        while(n > 0){
+            int digit = n%10;
+
+            if(!result.contains(String.valueOf(digit))){
+                result = digit+result;
+            }
+            n = n/10;
+        }
+        System.out.println(result);
+    }
+}
